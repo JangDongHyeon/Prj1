eb866f0999f0420ecd17c04621771bb59d7be8a3
JangDong <JangDong@LAPTOP-OMLPJ8BB>
2018-11-30 15:02:35 +0900

    11번째

--- JangPrj1/src/main/java/com/spring/board/controller/BoardController.java
+++ JangPrj1/src/main/java/com/spring/board/controller/BoardController.java
@@ -1,13 +1,17 @@
 package com.spring.board.controller;
 
+import java.lang.ProcessBuilder.Redirect;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.admin.dvo.PageMaker;
 import com.spring.admin.dvo.SearchVO;
@@ -40,11 +44,19 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value="boardInsert",method=RequestMethod.POST)
-	public String boardInsertPOST(BoardVO vo,Model model) {
+	public String boardInsertPOST(BoardVO vo,RedirectAttributes rttr,HttpSession session) {
+		String id=(String)session.getAttribute("userId");
+		vo.setBid(id);
+		if(boardDAO.boardInsert(vo))
+		rttr.addFlashAttribute("msg","등록되었습니다");
 		
-		return "board/boardInsert";
+		
+		return "redirect:/board/boardInsert";
 	}
-	
+	@RequestMapping(value="boardUpdate",method=RequestMethod.GET)
+	public String boardUpdateGET() {
+		return "board/boardUpdate";
+	}
 	
 	
 	
